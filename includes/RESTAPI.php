f9b431a950924c49100b018fa496d37aa2216a05
Benjamin Klemencic <benjamin.klemencic@fau.de>
2020-05-20 17:57:37 +0200

    2.0.2 (cleanup RESTAPI.php - we have neither categories nor tags in this plugin)

--- includes/RESTAPI.php
+++ includes/RESTAPI.php
@@ -10,8 +10,8 @@ defined( 'ABSPATH' ) || exit;
 class RESTAPI {
     public function __construct() {
         add_action( 'rest_api_init', [$this, 'createPostMeta'] );
-        add_action( 'rest_api_init', [$this, 'createTaxDetails'] );
-        add_action( 'rest_api_init', [$this, 'createChildren'] );
+        // add_action( 'rest_api_init', [$this, 'createTaxDetails'] );
+        // add_action( 'rest_api_init', [$this, 'createChildren'] );
         add_action( 'rest_api_init', [$this, 'addFilters'] );
     }
 
@@ -69,80 +69,80 @@ class RESTAPI {
     
     public function addFilters() {
         add_filter( 'rest_synonym_query', [$this, 'addFilterParam'], 10, 2 );
-        add_filter( 'rest_synonym_category_query', [$this, 'addFilterParam'], 10, 2 );
-        add_filter( 'rest_synonym_tag_query', [$this, 'addFilterParam'], 10, 2 );
+        // add_filter( 'rest_synonym_category_query', [$this, 'addFilterParam'], 10, 2 );
+        // add_filter( 'rest_synonym_tag_query', [$this, 'addFilterParam'], 10, 2 );
     }
 
-    public function getChildrenCategories( $term ) {
-        $children = get_terms( 
-            array(
-                'taxonomy' => 'synonym_category',
-                'parent' => $term['id'] 
-            ));
-        $aRet = array();
-        foreach ( $children as $child ){
-            $aRet[] = $child->name;
-        }
-        return $aRet;
-    }
+    // public function getChildrenCategories( $term ) {
+    //     $children = get_terms( 
+    //         array(
+    //             'taxonomy' => 'synonym_category',
+    //             'parent' => $term['id'] 
+    //         ));
+    //     $aRet = array();
+    //     foreach ( $children as $child ){
+    //         $aRet[] = $child->name;
+    //     }
+    //     return $aRet;
+    // }
 
 
-    public function getFaqCategories( $post ) {
-        $cats = wp_get_post_terms( $post['id'], 'synonym_category', array( 'fields' => 'names') );
-        return $cats;
-    }
+    // public function getFaqCategories( $post ) {
+    //     $cats = wp_get_post_terms( $post['id'], 'synonym_category', array( 'fields' => 'names') );
+    //     return $cats;
+    // }
 
-    public function getFaqTags( $post ) {
-        return wp_get_post_terms( $post['id'], 'synonym_tag', array( 'fields' => 'names')  );
-    }
+    // public function getFaqTags( $post ) {
+    //     return wp_get_post_terms( $post['id'], 'synonym_tag', array( 'fields' => 'names')  );
+    // }
 
-    public function getTaxSource( $object ) {
-        return get_term_meta( $object['id'], 'source', TRUE );
-    }
+    // public function getTaxSource( $object ) {
+    //     return get_term_meta( $object['id'], 'source', TRUE );
+    // }
 
-    public function getTaxLang( $object ) {
-        return get_term_meta( $object['id'], 'lang', TRUE );
-    }
+    // public function getTaxLang( $object ) {
+    //     return get_term_meta( $object['id'], 'lang', TRUE );
+    // }
 
-    public function createTaxDetails() {
-        register_rest_field( 'synonym',
-            'synonym_category',
-            array(
-                'get_callback'    => [$this, 'getFaqCategories'],
-                'update_callback'   => null,
-                'schema'            => null,
-             )
-        );
-        register_rest_field( 'synonym',
-            'synonym_tag',
-            array(
-                'get_callback'    => [$this, 'getFaqTags'],
-                'update_callback'   => null,
-                'schema'            => null,
-             )
-        );
+    // public function createTaxDetails() {
+    //     register_rest_field( 'synonym',
+    //         'synonym_category',
+    //         array(
+    //             'get_callback'    => [$this, 'getFaqCategories'],
+    //             'update_callback'   => null,
+    //             'schema'            => null,
+    //          )
+    //     );
+    //     register_rest_field( 'synonym',
+    //         'synonym_tag',
+    //         array(
+    //             'get_callback'    => [$this, 'getFaqTags'],
+    //             'update_callback'   => null,
+    //             'schema'            => null,
+    //          )
+    //     );
 
-        $fields = array( 'synonym_category', 'synonym_tag' );
-        foreach( $fields as $field ){
-            register_rest_field( $field, 'source', array(
-                'get_callback'    => [$this, 'getTaxSource'],
-                'schema'          => null,
-            ));
-            register_rest_field( $field, 'lang', array(
-                'get_callback'    => [$this, 'getTaxLang'],
-                'schema'          => null,
-            ));
-        }            
-    }
+    //     $fields = array( 'synonym_category', 'synonym_tag' );
+    //     foreach( $fields as $field ){
+    //         register_rest_field( $field, 'source', array(
+    //             'get_callback'    => [$this, 'getTaxSource'],
+    //             'schema'          => null,
+    //         ));
+    //         register_rest_field( $field, 'lang', array(
+    //             'get_callback'    => [$this, 'getTaxLang'],
+    //             'schema'          => null,
+    //         ));
+    //     }            
+    // }
 
-    public function createChildren() {
-        register_rest_field( 'synonym_category',
-            'children',
-            array(
-                'get_callback'    => [$this, 'getChildrenCategories'],
-                'update_callback'   => null,
-                'schema'            => null,
-             )
-        );
-    }
+    // public function createChildren() {
+    //     register_rest_field( 'synonym_category',
+    //         'children',
+    //         array(
+    //             'get_callback'    => [$this, 'getChildrenCategories'],
+    //             'update_callback'   => null,
+    //             'schema'            => null,
+    //          )
+    //     );
+    // }
 }
