ad63b650149cb8ac3bd31dfec21f9db0f1dd556b
Benjamin Klemencic <benjamin.klemencic@fau.de>
2020-05-26 14:29:59 +0200

    2.0.3 (bugfix checkDomain())
    ['guid']["rendered"] didn't contain correct protocol on some websites
    
    -> checkDomain() now uses ["link"] instead

--- includes/API.php
+++ includes/API.php
@@ -11,14 +11,16 @@ class API {
     private $aAllCats = array();
 
     protected function checkDomain( &$url ){
-        // checks if SYNONYM_ENDPOINT exists at $url, if HTTP-status is 200
+        // checks if ENDPOINT exists at $url, if HTTP-status is 200
         // returns (redirected) URL or FALSE  
         $ret = FALSE;
-        $request = wp_remote_get( $url . SYNONYM_ENDPOINT . '?per_page=1' );
+        $request = wp_remote_get( $url . ENDPOINT . '?per_page=1' );
         $status_code = wp_remote_retrieve_response_code( $request );
+
         if ( $status_code == '200' ){
             $content = json_decode( wp_remote_retrieve_body( $request ), TRUE );
-            $ret = substr( $content[0]['guid']["rendered"], 0 , strpos( $content[0]['guid']["rendered"], '?' ) );
+            // $ret = substr( $content[0]['guid']["rendered"], 0 , strpos( $content[0]['guid']["rendered"], '?' ) );
+            $ret = substr( $content[0]['link'], 0 , strpos( $content[0]['link'], '/faq' ) ) . '/';
         }
         return $ret;
     }
