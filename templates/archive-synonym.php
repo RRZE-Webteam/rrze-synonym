469047e1fff25b5455ae7210269355a66921a948
Barbara Bothe <barbara.bothe@fau.de>
2020-07-28 11:29:35 +0200

    Templates Single/Archive

--- templates/archive-synonym.php
+++ templates/archive-synonym.php
@@ -3,7 +3,35 @@
 Template Name: CPT synonym Archive Template
 */
 
+$fau_themes = [
+    'FAU-Einrichtungen',
+    'FAU-Einrichtungen-BETA',
+    'FAU-Medfak',
+    'FAU-RWFak',
+    'FAU-Philfak',
+    'FAU-Techfak',
+    'FAU-Natfak'
+];
+$theme = wp_get_theme();
+$fau = in_array($theme->Name, $fau_themes) ? true : false;
+
 get_header();
+
+if ($fau) {
+    get_template_part('template-parts/hero', 'index'); ?>
+    <div id="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <main id="droppoint">
+                        <h1 class="screen-reader-text"><?php echo __('Index','fau'); ?></h1>
+<?php } else { ?>
+    <div id="sidebar" class="sidebar">
+        <?php get_sidebar(); ?>
+    </div>
+    <div id="primary" class="content-area">
+		<main id="main" class="site-main">
+<?php }
 
 while ( have_posts() ){
     the_post();
@@ -13,5 +41,15 @@ while ( have_posts() ){
     echo '---- <br>';
 }
 
+if ($fau) { ?>
+                    </main>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } else { ?>
+        </main>
+    </div>
+<?php }
 get_footer();
 
--- templates/single-synonym.php
+++ templates/single-synonym.php
@@ -3,12 +3,54 @@
 Template Name: CPT synonym Single Template
 */
 
+$fau_themes = [
+    'FAU-Einrichtungen',
+    'FAU-Einrichtungen-BETA',
+    'FAU-Medfak',
+    'FAU-RWFak',
+    'FAU-Philfak',
+    'FAU-Techfak',
+    'FAU-Natfak'
+];
+$theme = wp_get_theme();
+$fau = in_array($theme->Name, $fau_themes) ? true : false;
+
 get_header();
 
 global $post;
 
+if ($fau) {
+    get_template_part('template-parts/hero', 'small'); ?>
+    <main id="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <main id="droppoint">
+                        <h1 class="screen-reader-text"><?php echo $post->post_title; ?></h1>
+<?php } else { ?>
+    <div id="sidebar" class="sidebar">
+        <?php get_sidebar(); ?>
+    </div>
+    <div id="primary" class="content-area">
+		<main id="main" class="site-main">
+<?php
+}
+echo '<div id="post-'. get_the_ID() . '" class="' . implode(' ', get_post_class()) .'">';
 echo '<strong>' . $post->post_title . '</strong><br>';
 echo '<strong>' . get_post_meta( $post->ID, 'synonym', TRUE ) . '</strong><br>';
 // echo 'In dieser Sprache wird der Titel ausgesprochen (nötig für z.B: < abbr title="Digital Subscriber Line" lang="en" >DSL< /abbr >) : <strong>' . DEFAULTLANGCODES[get_post_meta( $post->ID, 'titleLang', TRUE )] . '</strong><br>';
+echo '</div>';
+
+if ($fau) { ?>
+                    </main>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php get_template_part('template-parts/footer', 'social');
+} else { ?>
+        </main>
+    </div>
+<?php }
 
 get_footer();
